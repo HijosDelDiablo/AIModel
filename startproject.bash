d82cd72239015afa1a3530fd33e797aa8d1c6a20
joel-briones-B-Low <joellbriones701@gmail.com>
2025-11-05 15:03:01 -0600

    refactor: reorganize database cleanup script to ensure proper service startup

--- startproject.bash
+++ startproject.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 echo "Borrando la base de datos AI-ModelDB..."
+sudo chown -R $USER:$USER .
+sudo docker start ollama
+sudo docker start AI-MODEL
+
 mongosh "mongodb://localhost:27017/AI-ModelDB" --eval "db.dropDatabase()"
 echo "Base de datos limpia."
 fastapi dev main.py
