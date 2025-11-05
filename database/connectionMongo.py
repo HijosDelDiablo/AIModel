6b1e5e921e87d52fb67e292a2ff4518dba1e0580
joel-briones-B-Low <joellbriones701@gmail.com>
2025-11-05 12:20:17 -0600

    refactor database connection to use mongoengine instead of pymongo

--- database/connectionMongo.py
+++ database/connectionMongo.py
@@ -1,10 +1,10 @@
 # connection file
 
-from pymongo import MongoClient
+from mongoengine import connect
 
 def get_database():
 
     CONNECTION_STRING = "mongodb://localhost:27017"
-    client = MongoClient(CONNECTION_STRING)
+    client = connect(host=CONNECTION_STRING, db='AI-ModelDB')
     print("Connected to MongoDB, ", client)
-    return client['AI-ModelDB']
+    return client
