6992c13b720785b4eca614bf833ed194bb728afe
joel-briones-B-Low <joellbriones701@gmail.com>
2025-11-05 12:25:11 -0600

    add initial steps to router

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     db = get_database()
     
     llm = OllamaLLM(model="mistral", base_url="http://localhost:11434")
+    
     userChainInstance = user_chain(llm, question="Cuantos usuarios hay en la base de datos?", knowledge_base="usercount: 1500")
     print("Ejecutando la cadena de usuario...")
     print(userChainInstance)
