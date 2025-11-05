a96ddcf55a7750997edb2f36c5b7e402aab1502d
joel-briones-B-Low <joellbriones701@gmail.com>
2025-11-05 14:28:36 -0600

    refactor Session model: update metadata default value to None and adjust import statements

--- models/Session.py
+++ models/Session.py
@@ -2,15 +2,15 @@ from datetime import datetime
 
 from mongoengine import Document, StringField, EmailField, IntField, DateField, ReferenceField, ListField
 
-from models import Metadata, Message
-
+from .Message import  Message
+from .Metadata import Metadata
 from .User import User
 
 class Session(Document):
     # RELATIONS
     user = ReferenceField(User)
     messages = ListField(ReferenceField(Message), required=False, default=[])
-    metadata = ReferenceField(Metadata, required=False, default=Metadata)
+    metadata = ReferenceField(Metadata, required=False, default=None)
 
 
     # attributes
